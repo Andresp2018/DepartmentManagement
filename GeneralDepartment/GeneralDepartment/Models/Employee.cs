0ba1c23856e6a0e105c6bf52780caea94ff5eedc
Andre Pires <Andre.Silva.Pires@formandos.cinel.pt>
2021-02-16 15:13:29 +0000

    Init Commit - Classes and Controllers

---
+++ GeneralDepartment/GeneralDepartment/Models/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GeneralDepartment.Models
+{
+    public class Employee
+    {
+        public int EmplyeeID { get; set; }
+        public string EmployeeName { get; set; }
+        public string Department { get; set; }
+        public int DateJoin { get; set; }
+        public string ProfilePhoto { get; set; }
+    }
+}
---
+++ Olamundo/Olamundo/Form1.Designer.cs
@@ -0,0 +1,60 @@
+﻿
+namespace Olamundo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tb_numeros = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // tb_numeros
+            // 
+            this.tb_numeros.Location = new System.Drawing.Point(39, 31);
+            this.tb_numeros.Name = "tb_numeros";
+            this.tb_numeros.Size = new System.Drawing.Size(727, 248);
+            this.tb_numeros.TabIndex = 0;
+            this.tb_numeros.Text = "";
+            this.tb_numeros.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tb_numeros);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox tb_numeros;
+    }
+}
